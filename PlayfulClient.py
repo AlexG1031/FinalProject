163feebce7bbb9826da45aa5960eb8cb12a3f565
AlexG1031 <42430291+AlexG1031@users.noreply.github.com>
2020-07-07 02:55:03 -0400

    Making it look more presentable

--- PlayfulClient.py
+++ PlayfulClient.py
@@ -1,4 +1,19 @@
 from tkinter import *
+from tkinter import ttk
+
+options = [
+    "Alex",
+    "Jim",
+    "Tom",
+]
+
+def comboclick(event):
+    Label(frame, text=myCombo.get()).pack()
+
+def onReturn(event):
+    myLabel = Label(frame, text=name + ": " + e.get())
+    myLabel.pack()
+    e.delete(0, 'end')
 
 root = Tk()
 root.title("Client")
@@ -7,16 +22,9 @@ name = "Avatar Aang"
 frame = LabelFrame(root, text="Group Message", padx=100, pady=200)
 frame.pack(padx=10, pady=10)
 
-click=StringVar()
-list = OptionMenu(frame,click, "Alpha", "Beta", "Gamma")
-list.config(font=("Arial", 15))
-list.pack()
-
-def onReturn(event):
-    myLabel = Label(frame, text=name + ": " + e.get())
-    myLabel.pack()
-    e.delete(0, 'end')
-
+myCombo = ttk.Combobox(frame, value=options)
+myCombo.current(0)
+myCombo.pack()
 
 e = Entry(frame, width=30)
 e.insert(0, "Send message: ")
