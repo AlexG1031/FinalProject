1d974da7555c94d612119595b48a3de7ec4fad50
AlexG1031 <42430291+AlexG1031@users.noreply.github.com>
2020-07-08 13:59:29 -0400

    Just about done with the GUI :)

--- cleanClient.py
+++ cleanClient.py
@@ -25,10 +25,10 @@ class App:
         self.comboBox()
         self.text()
         self.text1.bind("<Return>", lambda x: self.onReturn())
-        self.comboBox1.bind("Click", lambda x: self.comboclick())
+        self.comboBox1.bind("<<ComboboxSelected>>", lambda x: self.comboclick())
 
     def frame(self):
-        #TODO: be sure frame1 doesn't go over the other frames
+        #TODO: be sure frame1 has a scroll to see past msgs
         self.frame1 = Frame(self.root) # Message box
         self.frame2 = Frame(self.root) # To: "___"
         self.frame3 = Frame(self.root) # TypeBox
@@ -43,10 +43,8 @@ class App:
 
 
     def label(self):
-        # self.label1 = Label(self.frame1, text=self.conv_texts[0])
-        self.label2= Label(self.frame2, text="To: ")
-        # self.label1.pack(side="left")
-        self.label2.pack(side="left", fill=X)
+        self.label1= Label(self.frame2, text="To: ")
+        self.label1.pack(side="left", fill=X)
 
     def comboBox(self):
         self.comboBox1 = ttk.Combobox(self.frame2, value=self.options)
@@ -63,20 +61,20 @@ class App:
 
         self.listbox1.destroy()
         self.listbox1 = Listbox(self.frame1)
+        self.listbox1.pack(side="left", fill=BOTH, expand=1)
 
         index = self.options.index(self.comboBox1.get())
         self.conv_texts[index].append(msg)
         for past_msg in self.conv_texts[index]:
             self.listbox1.insert(END, past_msg)
-        self.listbox1.pack(side="left", fill=BOTH, expand=1)
 
     def comboclick(self):
-        # TODO: have this work properly so that changing the thing within
-        #       the comboBox also changes the text displayed on top
-        print("Hello, Zuko here")
-        self.label1 = Label(self.frame1, text=self.comboBox1.get())
-        self.label1.pack(side="left")
-        self.onReturn()
+        self.listbox1.destroy()
+        self.listbox1 = Listbox(self.frame1)
+        self.listbox1.pack(side="left", fill=BOTH, expand=1)
+        index = self.options.index(self.comboBox1.get())
+        for past_msg in self.conv_texts[index]:
+            self.listbox1.insert(END, past_msg)
 
 root = Tk()
 root.title("Group Chat")
