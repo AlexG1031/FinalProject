e40e28d7a6201ede976eeb347e5af82f12c8e96c
AlexG1031 <42430291+AlexG1031@users.noreply.github.com>
2020-07-08 04:46:38 -0400

    much better, still cleaning up

--- cleanClient.py
+++ cleanClient.py
@@ -21,41 +21,44 @@ class App:
         self.name = "Avatar Aang"
         self.frame()
         self.label()
-        self.combo()
         self.text()
         self.text1.bind("<Return>", lambda x: self.onReturn())
+        # self.comb
 
     def frame(self):
-        self.frame1 = LabelFrame(root, text="Group Message")
-        self.frame1.pack(padx=10, pady=10, fill=None, expand=False)
+        self.frame1 = Frame(self.root) # Message box
+        self.frame2 = Frame(self.root) # To: "___"
+        self.frame3 = Frame(self.root) # TypeBox
+
+        self.frame1.pack(fill=X)
+        self.frame2.pack(fill=X)
+        self.frame3.pack(fill=X)
 
     def label(self):
-        self.label1 = Label(self.frame1, text="To: ")
-        self.label1.pack()
-
-    def combo(self):
-        self.myCombo1 = ttk.Combobox(self.frame1, value=self.options)
-        self.myCombo1.current(0)
-        self.myCombo1.pack()
+        self.label1= Label(self.frame2, text="To: ")
+        self.label1.pack(side="left", fill=X)
+        self.comboBox1 = ttk.Combobox(self.frame2, value=self.options)
+        self.comboBox1.current(0)
+        self.comboBox1.pack(side="left", fill=X)
 
     def text(self):
-        self.text1 = Text(self.frame1, height=5, width=50)
-        self.text1.pack(ipady=3)
+        self.text1 = Text(self.frame3, width=40, height=5)
+        self.text1.pack(side="left")
 
-    def comboclick(self):
-        self.label1 = Label(self.frame1, text=self.myCombo1.get()).pack()
+    # def comboclick(self):
+    #     self.label1 = Label(self.frame1, text=self.comboBox1.get())
+    #     self.label1.pack()
 
     def onReturn(self):
-        print("starting at onReturn")
-        index = self.options.index(self.myCombo1.get())
+        index = self.options.index(self.comboBox1.get())
         self.conv_texts[index] = self.conv_texts[index] + self.name + ": " + self.text1.get("0.0", "end")
-        self.myLabel = Label(self.frame1, text=self.conv_texts[index])
-        self.myLabel.pack()
+        self.label2 = Label(self.frame1, text=self.conv_texts[index])
+        self.label2.pack(fill=X)
         self.text1.delete("0.0", "end")
 
 root = Tk()
-root.title("Client")
-root.geometry("800x800")
+root.title("Group Chat")
+root.geometry("300x600")
 
 app = App(root)
 root.mainloop()
