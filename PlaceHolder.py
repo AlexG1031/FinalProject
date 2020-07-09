3bae65c067d36c719799b9834535cc38abf118f0
AlexG1031 <42430291+AlexG1031@users.noreply.github.com>
2020-07-09 04:49:54 -0400

    got multithreading code within PlaceHolder.py, just need to fix it...

--- PlaceHolder.py
+++ PlaceHolder.py
@@ -3,6 +3,8 @@ from tkinter import ttk, Text
 import socket
 import errno
 import sys
+import threading
+from queue import Queue
 
 class App:
     options = [
@@ -22,6 +24,11 @@ class App:
     IP = "127.0.0.1"
     PORT = 1234
     my_username = input("Username: ")
+    NUMBER_OF_THREADS = 2
+    JOB_NUMBER = [1, 2]
+    queue = Queue()
+    all_connections = []
+    all_address = []
 
     def __init__(self, root):
         self.root = root
@@ -34,6 +41,9 @@ class App:
         self.text1.bind("<Return>", lambda x: self.onReturn())
         self.comboBox1.bind("<<ComboboxSelected>>", lambda x: self.comboclick())
         self.set_up_connection()
+        self.create_workers()
+        self.create_jobs()
+        self.work()
 
     def frame(self):
         # TODO: be sure frame1 has a scroll to see past msgs
@@ -75,11 +85,51 @@ class App:
         for past_msg in self.conv_texts[index]:
             self.listbox1.insert(END, past_msg)
 
-        # Socket Programming
+    def comboclick(self):
+        self.listbox1.destroy()
+        self.listbox1 = Listbox(self.frame1)
+        self.listbox1.pack(side="left", fill=BOTH, expand=1)
+        index = self.options.index(self.comboBox1.get())
+        for past_msg in self.conv_texts[index]:
+            self.listbox1.insert(END, past_msg)
+
+    def set_up_connection(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((self.IP, self.PORT))
+        self.client_socket.setblocking(False)
+        self.username = self.my_username.encode('utf-8')
+        username_header = f"{len(self.username):<{self.HEADER_LENGTH}}".encode('utf-8')
+        self.client_socket.send(username_header + self.username)
+    # Create worker threads
+    def create_workers(self):
+        for _ in range(self.NUMBER_OF_THREADS):
+            t = threading.Thread(target=self.work)
+            t.daemon = True
+            t.start()
+
+
+    # Do next job that is in the queue (send message or display new message)
+    def work(self):
         while True:
-            self.msg = self.msg.encode('utf-8')
-            msg_header = f"{len(self.msg):<{self.HEADER_LENGTH}}".encode('utf-8')
-            self.client_socket.send(msg_header + self.msg)
+            x = self.queue.get()
+            if x == 1:
+                self.send_message()
+            if x == 2:
+                self.display_recv_message()
+
+            self.queue.task_done()
+    def create_jobs(self):
+        for x in self.JOB_NUMBER:
+            self.queue.put(x)
+
+        self.queue.join()
+    def send_message(self):
+        while True:
+            message = input(f'{self.my_username} > ')
+            if message:
+                message = message.encode('utf-8')
+                message_header = f"{len(message):<{self.HEADER_LENGTH}}".encode('utf-8')
+                self.client_socket.send(message_header + message)
             try:
                 while True:
                     username_header = self.client_socket.recv(self.HEADER_LENGTH)
@@ -104,24 +154,38 @@ class App:
             except Exception as e:
                 print('Reading error: '.format(str(e)))
                 sys.exit()
+    def display_recv_message(self):
+        while True:
+            # time = 0.00125
+            message = ""
+            if message:
+                message = message.encode('utf-8')
+                message_header = f"{len(message):<{self.HEADER_LENGTH}}".encode('utf-8')
+                self.client_socket.send(message_header + message)
+            try:
+                while True:
+                    username_header = self.client_socket.recv(self.HEADER_LENGTH)
+                    if not len(username_header):
+                        print('Connection closed by the server')
+                        sys.exit()
+                    username_length = int(username_header.decode('utf-8').strip())
+                    username = self.client_socket.recv(username_length).decode('utf-8')
 
+                    message_header = self.client_socket.recv(self.HEADER_LENGTH)
+                    message_length = int(message_header.decode('utf-8').strip())
+                    message = self.client_socket.recv(message_length).decode('utf-8')
 
-    def comboclick(self):
-        self.listbox1.destroy()
-        self.listbox1 = Listbox(self.frame1)
-        self.listbox1.pack(side="left", fill=BOTH, expand=1)
-        index = self.options.index(self.comboBox1.get())
-        for past_msg in self.conv_texts[index]:
-            self.listbox1.insert(END, past_msg)
+                    print(f'{username} > {message}')
 
-    def set_up_connection(self):
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect((self.IP, self.PORT))
-        self.client_socket.setblocking(False)
-        self.username = self.my_username.encode('utf-8')
-        username_header = f"{len(self.username):<{self.HEADER_LENGTH}}".encode('utf-8')
-        self.client_socket.send(username_header + self.username)
+            except IOError as e:
+                if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+                    print('Reading error: {}'.format(str(e)))
+                    sys.exit()
+                continue
 
+            except Exception as e:
+                print('Reading error: '.format(str(e)))
+                sys.exit()
 
 root = Tk()
 root.title("Group Chat")
